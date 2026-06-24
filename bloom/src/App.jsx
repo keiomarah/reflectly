c52fc434a450f8e965d3de9011f103f8ab752dd0
keiomarah <keiomarah@hotmail.com>
2026-06-24 21:24:24 +0200

    update app.jsx

--- bloom/src/App.jsx
+++ bloom/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
-import { Home } from "./pages/home";
 import { Account } from "./pages/account";
 import { LoginPage } from "./pages/LoginPage";
 import { SignUpPage } from "./pages/SignUpPage";
